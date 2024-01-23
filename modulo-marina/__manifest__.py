980c2004b2bf0cf464011d20e0685ffb0e5d77b6
mmarm09 <marina04mart@gmail.com>
2024-01-23 18:55:33 +0100

    último manifest ya si que si

--- modulo-marina/__manifest__.py
+++ modulo-marina/__manifest__.py
@@ -1,22 +1,33 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Modulo SGE",
-    'version': '1.0',
-    'depends': ['base'],
-    'author': "Marina Martín",
-    'category': 'Category',
+    'name': "modulo-marina",
+
+    'summary': "El primer módulo de la clase de SGE",
+
     'description': """
         Este módulo ha sido creado con scaffolding y está sincronizado en mi repositorio Github
     """,
-    # data files always loaded at installation
-    'data': [
-        'views/mymodule_view.xml',
-    ],
-    # data files containing optionally loaded demonstration data
-    'demo': [
-        'demo/demo_data.xml',
-    ],
-    "application": True,
-    "installable": True,
-}
 
+    'author': "Marina Martín",
+    'website': "https://www.ieshlanz.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
